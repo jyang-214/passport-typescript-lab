14e75cb15d6f8ed56e69455f5c3fcd2cfa4e2fce
jyang-214 <jyang214@my.bcit.ca>
2023-06-22 19:45:44 -0700

    Final Assignment

--- middleware/passportStrategies/githubStrategy.ts
+++ middleware/passportStrategies/githubStrategy.ts
@@ -1,21 +1,29 @@
-import { Strategy as GitHubStrategy } from 'passport-github2';
-import { PassportStrategy } from '../../interfaces/index';
+import { Strategy as GitHubStrategy } from "passport-github2";
+import { PassportStrategy } from "../../interfaces/index";
+import { Request } from "express";
+import { Profile } from "passport";
 
 const githubStrategy: GitHubStrategy = new GitHubStrategy(
-    {
-        clientID: "",
-        clientSecret: "",
-        callbackURL: "",
-        passReqToCallback: true,
-    },
-    
-    /* FIX ME 😭 */
-    async (req: any, accessToken: any, refreshToken: any, profile: any, done: any) => {},
+	{
+		clientID: "",
+		clientSecret: "",
+		callbackURL: "",
+		passReqToCallback: true,
+	},
+
+	/* FIXED 😭 */
+	async (
+		req: Request,
+		accessToken: string,
+		refreshToken: string,
+		profile: Profile,
+		done: any
+	) => {}
 );
 
 const passportGitHubStrategy: PassportStrategy = {
-    name: 'github',
-    strategy: githubStrategy,
+	name: "github",
+	strategy: githubStrategy,
 };
 
 export default passportGitHubStrategy;
